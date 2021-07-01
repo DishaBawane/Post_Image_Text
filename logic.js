0b9b9064cf3b415d63b51063ef2435bfd3ecf193
amol bawane <dishakhode@gmail.com>
2021-07-01 09:27:40 +0530

    second commit

--- logic.js
+++ logic.js
@@ -6,67 +6,96 @@ let postBtn = document.querySelector(".postbtn");
 let usertext;
 
 
-let postObj = [
-    {
-      text: 'Beautiful Nature Image',
-      img: 'https://source.unsplash.com/1600x900/?nature,water'
-    },
-  {
-      text: 'Greyscale Image',
-      img: 'https://picsum.photos/200/300?grayscale'
-    },
-  {
-      text: 'Dog Image',
-      img: 'https://picsum.photos/id/237/200/300'
-    }
-  ]
-
-  userEnterText.value = postObj[0].text;
-  userEnterUrl.value = postObj[0].img;
-
-  userEnterText.value = postObj[1].text;
-  userEnterUrl.value = postObj[1].img;
-
-  userEnterText.value = postObj[2].text;
-  userEnterUrl.value = postObj[2].img;
+/* create a card html using  JAVASCRIPT*/
+let bodyElement=document.body;
+let cardElement=document.createElement('div');
+let imageContainer=document.createElement('div');
+let infoContainer=document.createElement('div');
+let imageElement=document.createElement('img')
+let paragraphElement=document.createElement('p');
 
 
+/*adding classes to above declared element*/
+cardElement.className="card";
+imageContainer.className="image-container";
+infoContainer.className="text";
+imageElement.className="image";
+paragraphElement.className="userinput";
 
 
-postBtn.addEventListener('click',()=>{
+imageElement.src="https://source.unsplash.com/random";
+imageElement.setAttribute=("alt","Image from Unsplash");
+paragraphElement.innerText="Beautiful, free images and photos that you can download and use for any project";
+
+
+bodyElement.appendChild(cardElement);
+cardElement.append(imageContainer,infoContainer)
+imageContainer.appendChild(imageElement);
+infoContainer.appendChild(paragraphElement);
+
+
+
+// let postObj = [
+//     {
+//       text: 'Beautiful Nature Image',
+//       img: 'https://source.unsplash.com/1600x900/?nature,water'
+//     },
+//   {
+//       text: 'Greyscale Image',
+//       img: 'https://picsum.photos/200/300?grayscale'
+//     },
+//   {
+//       text: 'Dog Image',
+//       img: 'https://picsum.photos/id/237/200/300'
+//     }
+//   ]
+
+//   userEnterText.value = postObj[0].text;
+//   userEnterUrl.value = postObj[0].img;
+
+//   userEnterText.value = postObj[1].text;
+//   userEnterUrl.value = postObj[1].img;
+
+//   userEnterText.value = postObj[2].text;
+//   userEnterUrl.value = postObj[2].img;
+
+
+
+
+// postBtn.addEventListener('click',()=>{
   
-    display();
-    displayimage();
+//     display();
+//     displayimage();
  
-   userEnterText.value = " ";
-   userEnterUrl.value = " ";
+//    userEnterText.value = " ";
+//    userEnterUrl.value = " ";
 
 
-});
+// });
 
 
 
-function display(){
+// function display(){
 
-  userEnterText =  document.getElementById("usertext").value; 
- // textDisplay.innerHTML = userEnterText;
-  let input = document.createElement("P");
-  let txt = document.createTextNode( userEnterText);
-  textDisplay.appendChild(txt);
-  document.body.appendChild(input);
-}
+//   userEnterText =  document.getElementById("usertext").value; 
+//  // textDisplay.innerHTML = userEnterText;
+//   let input = document.createElement("P");
+//   let txt = document.createTextNode( userEnterText);
+//   textDisplay.appendChild(txt);
+//   document.body.appendChild(input);
+// }
 
 
-function displayimage(){
+// function displayimage(){
 
-    userEnterUrl = document.querySelector(".userimgurl").value
-    imgDisplay = document.querySelector(".image");
-    let img =  document.createElement("IMG");
-    img.src = userEnterUrl;
-    img.width=515;
-    img.height=273;
-    imgDisplay.appendChild(img);
-}
+//     userEnterUrl = document.querySelector(".userimgurl").value
+//     imgDisplay = document.querySelector(".image");
+//     let img =  document.createElement("IMG");
+//     img.src = userEnterUrl;
+//     img.width=515;
+//     img.height=273;
+//     imgDisplay.appendChild(img);
+// }
 
 
 
